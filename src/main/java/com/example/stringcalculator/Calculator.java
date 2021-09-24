f32f80acfddbed5707a9d4e6d3644a8f691cb7b3
satya <satya.p91@yahoo.com>
2021-09-24 14:57:33 +0530

    modified code to pass test 1,2 & 3

--- src/main/java/com/example/stringcalculator/Calculator.java
+++ src/main/java/com/example/stringcalculator/Calculator.java
@@ -5,7 +5,11 @@ public class Calculator {
 	public static int add(String numbers) {
 		if(numbers.isEmpty()) {
 			return 0;
+		}else if(numbers.contains(",")) {
+			String[] t = numbers.split(",");
+			return Integer.parseInt(t[0]) + Integer.parseInt(t[1]);
+		}else {
+			return Integer.parseInt(numbers);
 		}
-		return Integer.parseInt(numbers);
 	}
 }
