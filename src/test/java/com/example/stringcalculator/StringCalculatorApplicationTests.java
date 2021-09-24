4476c557ac39d41885da00fc581026a597aa1e16
satya <satya.p91@yahoo.com>
2021-09-24 14:53:34 +0530

    create 3nd test and it failed

--- src/test/java/com/example/stringcalculator/StringCalculatorApplicationTests.java
+++ src/test/java/com/example/stringcalculator/StringCalculatorApplicationTests.java
@@ -11,15 +11,23 @@ class StringCalculatorApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	//test 1
 	@Test
 	void shoudReturnZeroWhenEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 	
+	//test 2
 	@Test
 	void shouldReturnNumberWhenNumber() {
 		assertEquals(1, Calculator.add("1"));
+	}
+	
+	//test3
+	@Test
+	void shouldReturnSumOnTwoNumberDelimitedByComma() {
+		assertEquals(3, Calculator.add("1,2"));
 	}
 	
 }
